894e3a692655653b7f642c25b4ff2a1642fea90d
Saksham Puri <sakshampuri1999@gmail.com>
2020-09-19 01:13:57 +0530

    Updated http sources with http to fix mixed content warning.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
         
         <p className="copyright">
-         Built with <a href="http://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="http://www.sakshampuri.com"> Saksham Puri</a>  
+         Built with <a href="https://gatsby-dimension.surge.sh">Gatsby.js</a> and ❤️ by <a href="https://www.sakshampuri.com"> Saksham Puri</a>  
         </p>
     </footer>
 )
