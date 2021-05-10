b37b45fc11f763f853d5aa9ef12f33d9d634bf71
Saksham Puri <sakshampuri1999@gmail.com>
2021-05-10 18:02:25 +0530

    404 page edited

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
 
+const NotFoundPage = () => {
+    const [catUrl, changeCatUrl] = React.useState("");
 
-const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist.... Wanna head back <a href="https://www.adityaketkar.me">Home</a> ?</p>
-    <p>[ If you're here for Alexa Companion, I had to take it down due to some issues. Contact me <a href="https://www.adityaketkar.me">here</a> to get it offline. ]</p> 
-    <hr></hr>
-    
-    <h2>Don't be sad, it's CATURDAY!</h2>
-    <p>Here is a <strong>truly</strong> random cat for you :)</p> 
-    <img src="https://theoldreader.com/kittens/600/400"></img>
-  </Layout>
-)
+    React.useLayoutEffect(() => {
+        fetch("https://api.thecatapi.com/v1/images/search")
+            .then((res) => res.json())
+            .then((res) => {
+                if (res && res[0]) changeCatUrl(res[0].url);
+            });
+    }, []);
 
-export default NotFoundPage
+    return (
+        <Layout>
+            <h1>NOT FOUND</h1>
+            <p>
+                You just hit a route that doesn&#39;t exist.... Wanna head back{" "}
+                <a href='https://sakshampuri.com'>Home</a> ?
+            </p>
+            <hr></hr>
+
+            <h2>Don't be sad, it's CATURDAY!</h2>
+            <p>
+                Here is a <strong>truly</strong> random cat for you :)
+            </p>
+            {catUrl && <img src={catUrl}></img>}
+        </Layout>
+    );
+};
+
+export default NotFoundPage;
